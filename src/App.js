0ac1fc80fa6e006e509fa668bbe75aa82eaabb57
krood20 <kylerood16@gmail.com>
2024-06-24 16:16:32 -0700

    added nav bar and better styling on home page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
+// App.js
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
 import Photography from './components/Photography';
 import Clothing from './components/Clothing';
 import Cooking from './components/Cooking';
+import NavBar from './components/NavBar'; // Import NavBar component
 import './App.css';
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <nav>
-          <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/photography">Photography</Link></li>
-            <li><Link to="/clothing">Clothing</Link></li>
-            <li><Link to="/cooking">Cooking</Link></li>
-          </ul>
-        </nav>
+        <NavBar /> {/* Include NavBar component here */}
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/photography" element={<Photography />} />
