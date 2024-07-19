e9a49b90a4cb827ba5de13daac4c5fc3661a40dd
krood20 <kylerood16@gmail.com>
2024-07-19 14:13:36 -0700

    commented out rest of site, made it a simple react update

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,29 +1,9 @@
 // Home.js
 import React from 'react';
-import ImageGallery from 'react-image-gallery';
 import './Home.css'; // Import your CSS file for styling
-
-const images = [
-  {
-    original: '/images/img_bg_2.jpg',
-    thumbnail: '/images/img_bg_2.jpg',
-    description: 'Image 2'
-  },
-  {
-    original: '/images/img_bg_3.jpg',
-    thumbnail: '/images/img_bg_3.jpg',
-    description: 'Image 3'
-  },
-  {
-    original: '/images/img_bg_2.jpg',
-    thumbnail: '/images/img_bg_2.jpg',
-    description: 'Image 2'
-  }
-];
 
 const Home = () => {
   const handleDownload = () => {
-    // Replace 'resume.pdf' with the path to your actual resume file
     const resumePath = '/Kyle_Rood_Resume_2024_Freelance.pdf';
     window.open(resumePath, '_blank');
   };
@@ -36,8 +16,8 @@ const Home = () => {
         <div className="home__contact">
           <p>Email: kylerood16@gmail.com</p>
           <p>LinkedIn: <a href="https://www.linkedin.com/in/kyle-rood/" target="_blank" rel="noopener noreferrer">Kyle Rood</a></p>
-          <button className="home__download-btn" onClick={handleDownload}>Download Resume</button>
         </div>
+        <button className="home__download-btn" onClick={handleDownload}>Download Resume</button>
       </header>
     </div>
   );
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,10 +8,10 @@ const NavBar = () => {
     <nav className="navbar">
       <ul className="navbar__list">
         <li><Link to="/" className="navbar__link">Home</Link></li>
-        <li><Link to="/resume" className="navbar__link">Resume</Link></li>
+        {/* <li><Link to="/resume" className="navbar__link">Resume</Link></li>
         <li><Link to="/photography" className="navbar__link">Photography</Link></li>
         <li><Link to="/clothing" className="navbar__link">Clothing</Link></li>
-        <li><Link to="/cooking" className="navbar__link">Cooking</Link></li>
+        <li><Link to="/cooking" className="navbar__link">Cooking</Link></li> */}
       </ul>
     </nav>
   );
